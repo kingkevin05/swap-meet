d3a19497f5a909e748885c436723d9862c81d00c
Noah <noahschnur@gmail.com>
2021-05-06 19:26:31 -0700

    bare bone push so I can pull

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS swapmeet_db;
+CREATE DATABASE swapmeet_db;
+
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE categories(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR (30),
+    -- web_page VARCHAR (255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE item(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    description VARCHAR(255),
+    image_url VARCHAR (255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES categories(id)
+);
+
+CREATE TABLE users(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    username VARCHAR (50),
+    password VARCHAR (30),
+    email VARCHAR(80),
+    phone_number INTEGER,
+    address VARCHAR(150),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES item(id),
+    -- FOREIGN KEY (id) REFERENCES categories(id)
+);
