401c7bb1eb207b451cdc21a31138ed977d056d6a
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-05-11 19:57:38 -0700

    got localhost running

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+const hbs = exphbs.create({
+  helpers: {
+    format_date: date => {
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+  }
+});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
