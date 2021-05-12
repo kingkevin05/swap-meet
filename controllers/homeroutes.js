9e3006c84fd166308da2a2e62014233965a695d0
Noah <noahschnur@gmail.com>
2021-05-11 20:18:52 -0700

    test

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,14 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD:routes/homeroutes.js
     
 });
 
 module.exports = router;
-=======
-    res.render('login')
-})
-
-module.exports = router
->>>>>>> 401c7bb1eb207b451cdc21a31138ed977d056d6a:controllers/homeroutes.js
