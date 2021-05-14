9bb83b460e7b142a863356226cee2c1ac7e88881
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-05-13 23:48:02 -0700

    minor edits

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-const User = require("./User");
-const Item = require("./Item");
-const Categories = require("./Categories");
+const User = require('./User');
+const Item = require('./Item');
+const Categories = require('./Categories');
 
-User.hasMany(Item, { foreignKey: "user_id", onDelete: "CASCADE" });
+User.hasMany(Item, {foreignKey: "user_id", onDelete: "CASCADE"});
 
-Categories.hasMany(Item, { foreignKey: "categories_id" });
+Categories.hasMany(Item, {foreignKey: "categories_id"});
 
-Item.belongsTo(User, { foreignKey: "user_id" });
+Item.belongsTo(User, {foreignKey: "user_id"});
 
-Item.belongsTo(Categories, { foreignKey: "categories_id" });
+Item.belongsTo(Categories, {foreignKey: "categories_id"});
 
 module.exports = { User, Item, Categories };
