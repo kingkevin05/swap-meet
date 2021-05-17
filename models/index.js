2d79a04dde080ac63d2742b99440337345e79110
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-05-16 19:47:07 -0700

    index fix

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Item = require('./Item');
 const Categories = require('./Categories');
-const Image = require('./Image');
 
 User.hasMany(Item, {foreignKey: "user_id", onDelete: "CASCADE"});
 
@@ -11,6 +10,4 @@ Item.belongsTo(User, {foreignKey: "user_id"});
 
 Item.belongsTo(Categories, {foreignKey: "categories_id"});
 
-// Image.belongsTo(Item, {foreignKey: "user_id"})
-
-module.exports = { User, Item, Categories, Image };
+module.exports = { User, Item, Categories };
