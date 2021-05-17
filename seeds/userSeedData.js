da0c6d4998116879bb74d2fc9462dc4bac8594ef
Noah <noahschnur@gmail.com>
2021-05-16 20:00:08 -0700

    added more fake users/items

--- seeds/userSeedData.js
+++ seeds/userSeedData.js
@@ -12,6 +12,36 @@ const userData = [
     phoneNumber: "5105558888",
     address: "1234 Main St, Berkeley, CA, 94702",
   },
+  {
+    id: "2",
+    firstName: "Kevin",
+    lastName: "Faker",
+    username: "fakekevin",
+    email: "fakekevin@gmail.com",
+    password: "guest",
+    phoneNumber: "5105557777",
+    address: "1235 Main St, Berkeley, CA, 94702",
+  },
+  {
+    id: "3",
+    firstName: "Lawrence",
+    lastName: "Fakest",
+    username: "fakestuser",
+    email: "fakestuser@gmail.com",
+    password: "guest",
+    phoneNumber: "5105556666",
+    address: "1236 Main St, Berkeley, CA, 94702",
+  },
+  {
+    id: "4",
+    firstName: "Manny",
+    lastName: "Unreal",
+    username: "mannyisthebest",
+    email: "mannyr@gmail.com",
+    password: "guest",
+    phoneNumber: "5105554444",
+    address: "1237 Main St, Berkeley, CA, 94702",
+  }
 ];
 
 const userSeedData = () => User.bulkCreate(userData, { individualHooks: true });
